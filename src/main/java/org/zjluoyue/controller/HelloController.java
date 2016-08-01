327e3f5064e3f7064b106319c2daa7251aebfe26
zjluoyue <1126088092@qq.com>
2016-08-01 14:21:16 +0800

    using spring boot creat a web app

--- src/main/java/org/zjluoyue/controller/HelloController.java
+++ src/main/java/org/zjluoyue/controller/HelloController.java
@@ -9,9 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by zjluoyue on 2016/7/30.
  */
 @Controller
+@RequestMapping(value = "/demo-web")
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String sayHello(ModelMap model) {
         model.addAttribute("greeting", "Hello Spring MVC Framework!");
         return "hello";
