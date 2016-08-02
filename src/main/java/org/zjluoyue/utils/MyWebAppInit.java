e8e7b1dbe0c77d827cdec1476750913fbe822d8a
zjluoyue <1126088092@qq.com>
2016-08-02 09:35:42 +0800

    基于代码的Servlet容器初始化

---
+++ src/main/java/org/zjluoyue/utils/MyWebAppInit.java
@@ -0,0 +1,25 @@
+package org.zjluoyue.utils;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by zjluoyue on 2016/8/2.
+ */
+public class MyWebAppInit implements WebApplicationInitializer{
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+        appContext.setConfigLocation("/WEB-INF/hello-servlet.xml");
+
+        ServletRegistration.Dynamic registration = servletContext
+                .addServlet("dispatcher", new DispatcherServlet(appContext));
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/demo-web");
+    }
+}
